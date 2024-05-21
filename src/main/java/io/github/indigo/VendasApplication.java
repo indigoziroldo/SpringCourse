ddc3f110798b942d2e5efd53bbff490e17900b80
indigo <indigoziroldo@gmail.com>
2024-05-21 15:56:45 -0300

    UPDATE VendasApplication

--- src/main/java/io/github/indigo/VendasApplication.java
+++ src/main/java/io/github/indigo/VendasApplication.java
@@ -1,5 +1,12 @@
 package io.github.indigo;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class VendasApplication {
-    // oi
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
 }
