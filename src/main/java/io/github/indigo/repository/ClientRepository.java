97e8e49c15086053e2f6fc75fb48b1b3712f3bef
indigo <indigoziroldo@gmail.com>
2024-05-23 13:34:43 -0300

    CREATE repository Client

---
+++ src/main/java/io/github/indigo/repository/ClientRepository.java
@@ -0,0 +1,12 @@
+package io.github.indigo.repository;
+
+import io.github.indigo.model.Client;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientRepository {
+    public void persist(Client client){
+        // has access to the data and save it
+    }
+
+}
