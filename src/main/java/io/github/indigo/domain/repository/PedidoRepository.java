6bb4bbc7bc8f60aba630ca36635e856b53458913
indigo <indigoziroldo@gmail.com>
2024-06-05 17:25:10 -0300

    refactor: create commented annotation

--- src/main/java/io/github/indigo/domain/repository/PedidoRepository.java
+++ src/main/java/io/github/indigo/domain/repository/PedidoRepository.java
@@ -3,6 +3,9 @@ package io.github.indigo.domain.repository;
 import io.github.indigo.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+// DAO =====>>>> Data Acess Only
+
+
 public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
 
