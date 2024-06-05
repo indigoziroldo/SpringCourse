f8df078c5c2e65259927264f0308eb5cab3d1b02
indigo <indigoziroldo@gmail.com>
2024-06-05 15:23:22 -0300

    feat: map produto entity

--- src/main/java/io/github/indigo/domain/entity/Pedido.java
+++ src/main/java/io/github/indigo/domain/entity/Pedido.java
@@ -1,15 +1,25 @@
 package io.github.indigo.domain.entity;
 
+import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "pedido")
 public class Pedido {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
+    @Column(name = "data_pedido")
     private LocalDate dataPedido;
 
+    @Column(name = "total", length = 20, precision = 2) //numeric(20,2) -> 1000.00
     private BigDecimal total;
 
     public Integer getId() {
