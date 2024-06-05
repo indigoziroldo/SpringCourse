dc9c86916c1eee35bf27ee3257074882248faaaa
indigo <indigoziroldo@gmail.com>
2024-06-05 16:02:31 -0300

    feat: map pedido entity with onetomany

--- src/main/java/io/github/indigo/domain/entity/Pedido.java
+++ src/main/java/io/github/indigo/domain/entity/Pedido.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "pedido")
@@ -21,6 +22,17 @@ public class Pedido {
 
     @Column(name = "total", length = 20, precision = 2) //numeric(20,2) -> 1000.00
     private BigDecimal total;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<ItemPedido> itens;
+
+    public List<ItemPedido> getItens() {
+        return itens;
+    }
+
+    public void setItens(List<ItemPedido> itens) {
+        this.itens = itens;
+    }
 
     public Integer getId() {
         return id;
