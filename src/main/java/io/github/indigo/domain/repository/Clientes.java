a7f654e500beab34b8824abdd520f2d706c09a12
indigo <indigoziroldo@gmail.com>
2024-06-05 13:37:56 -0300

    feat: using query on clientes

--- src/main/java/io/github/indigo/domain/repository/Clientes.java
+++ src/main/java/io/github/indigo/domain/repository/Clientes.java
@@ -2,16 +2,15 @@ package io.github.indigo.domain.repository;
 
 import io.github.indigo.domain.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 public interface Clientes extends JpaRepository<Cliente, Integer> {
 
 
-    List <Object> findByNomeLike(String nome);
-
-    List <Cliente> findByNomeOrIdOrderById(String nome, Integer id);
-
-   // Cliente findOneClienteByNome(String nome);
+    @Query(value = "select c from Cliente c where c.nome like :nome ")
+    List <Cliente> encontrarPorNome(@Param("nome") String nome);
 
     boolean existsByNome(String nome);
 
