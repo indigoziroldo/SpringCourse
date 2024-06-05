436e5f028b546872f1ee69d9cf84eccb1b4dce0a
indigo <indigoziroldo@gmail.com>
2024-06-05 16:31:39 -0300

    fix: rename ClienteRepository interface and update

--- src/main/java/io/github/indigo/VendasApplication.java
+++ src/main/java/io/github/indigo/VendasApplication.java
@@ -1,13 +1,12 @@
 package io.github.indigo;
 
 import io.github.indigo.domain.entity.Cliente;
-import io.github.indigo.domain.repository.Clientes;
+import io.github.indigo.domain.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.env.CommandLinePropertySource;
 
 import java.util.List;
 
@@ -16,7 +15,7 @@ import java.util.List;
 public class VendasApplication {
 
     @Bean
-    public CommandLineRunner init(@Autowired Clientes clientes){
+    public CommandLineRunner init(@Autowired ClienteRepository clientes){
         return args -> {
             System.out.println("Salvando clientes");
             clientes.save(new Cliente("Douglas"));
